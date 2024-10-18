1c797ea6364ee48977edee14279f40a2585290a0
IanStocker02 <iant.stocker@gmail.com>
2024-10-17 21:29:50 -0400

    updated the directory

--- server/middleware/authMiddleware.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  // Middleware logic here
-  next();
-};
-
-export default authMiddleware;
--- server/models/models.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING as string);
-
-export { sequelize };
