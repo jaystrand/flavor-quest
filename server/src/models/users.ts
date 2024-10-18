bff6c84fef682f72403cd66d3d9ff829d12f0bb7
Deepthi Sandeep <deepththampi@hotmail.com>
2024-10-17 21:47:19 -0500

    added user.ts 001

--- server/src/models/users.ts
+++ server/src/models/users.ts
@@ -1,38 +1,6 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+import { DataTypes,  Sequelize, Model, Optional } from 'sequelize';
 
-const User = sequelize.define('User', {
-    user_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,  
-        allowNull: false,
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,  
-        validate: {
-            isEmail: true,  
-        },
-    },
-    created_at: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,  
-    },
-}, {
-    timestamps: false, 
-    tableName: 'users',  
-});
+interface UsersAttributes{
+    
+}
 
-module.exports = User;
