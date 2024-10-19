e2ba1217eb27943441384826c6e51d075b7352a3
Deepthi Sandeep <deepththampi@hotmail.com>
2024-10-19 15:21:02 -0500

    added middleware

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface JwtPayload {
+  username: string;
+}
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  // TODO: verify the token exists and add the user data to the request object
+ // Get the authorization header from the request
+ const authHeader = req.headers.authorization;
+  // Check if the authorization header is present
+ if(authHeader){
+   // Extract the token from the authorization header
+  const token = authHeader.split(' ')[1];
+   // Get the secret key from the environment variables
+  const secretKey = process.env.JWT_SECRET_KEY||'';
+   // Verify the JWT token
+  jwt.verify(token,secretKey,(err,user)=>{
+    if(err){
+      return res.sendStatus(403); // forbidden -> token invalid
+    }
+    // Attach the user information to the request object
+    req.user = user as JwtPayload;
+    return next();
+  });
+  
+    }else{
+      res.sendStatus(401); // unauth status -> no authHeader
+    }
+};
