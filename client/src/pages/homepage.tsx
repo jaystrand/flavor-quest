0695a542a9ad0ba74ea09cbfc4de63a1505fe87d
Deepthi Sandeep <deepththampi@hotmail.com>
2024-10-30 16:41:02 -0500

    Added styles

--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -2,6 +2,7 @@ import { fetchImg } from '../api/API';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Login from '../components/Login';
+import '../styles/homepage.css'
 const homepage = () => {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
   const navigate = useNavigate(); //  hook to navigate b/w pages
@@ -19,20 +20,20 @@ const homepage = () => {
     navigate('/register'); // Navigate to the registration page
   };
   return (
-    <div>
+    <div className="homepage-container" >
     
      
       <h1>Welcome to Flavor Quest</h1>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '50px' }}>
+      <div className="homepage-content" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '50px' }}>
         {/* Image */}
         <div>
           {imageUrl ? (
-            <img src={imageUrl} alt="Random food from Unsplash" style={{ maxWidth: '400px', height: 'auto' ,borderRadius: '10px'}} />
+            <img className="homepage-image" src={imageUrl} alt="Random food from Unsplash" style={{ maxWidth: '400px', height: 'auto' ,borderRadius: '10px'}} />
           ) : (
             <p>Loading image...</p>
           )}
         </div>
-      <div style={{ width: '300px', padding: '20px', border: '1px solid #ccc', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+      <div className="login-signup-box" style={{ width: '300px', padding: '20px', border: '1px solid #ccc', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
         <Login />
         <br />
         <button onClick={handleRegister}>Sign Up</button><label> Sign Up if not a member</label> {/* Add Register Button */}
